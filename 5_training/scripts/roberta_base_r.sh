bf0759a0ab2670ec7b6a6e2d69e72a406c2d39f8
Yoonsik Park <park.yoonsik@icloud.com>
2021-04-21 18:51:08 -0500

    add relation only training

---
+++ 5_training/scripts/roberta_base_r.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# stop on all errors
+set -e
+
+# death after SIGINT
+trap 'exit' INT
+
+export MAX_LENGTH=512
+export BERT_MODEL=roberta-base
+export BATCH_SIZE=4
+export GRAD_ACCUM_SIZE=4
+export NUM_EPOCHS=10
+export CURRENT_DIR=${PWD}
+export LEARNING_RATE=0.00003
+export DATA_DIR=./3_iob_data/context/train_relation
+
+for i in {1..10}; do
+    export SEED=$i
+    export OUTPUT_DIR_NAME=roberta-base-final-$i
+    export OUTPUT_DIR=./5_training/saved_models_r/${OUTPUT_DIR_NAME}
+    mkdir -p $OUTPUT_DIR
+    ./5_training/scripts/run.sh
+done
+
