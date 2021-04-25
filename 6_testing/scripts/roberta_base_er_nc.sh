df36591ecc5c0f29f8883446ba7f08ba3ea651f3
Yoonsik Park <park.yoonsik@icloud.com>
2021-04-24 19:35:26 -0500

    add more testing scripts

---
+++ 6_testing/scripts/roberta_base_er_nc.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# stop on all errors
+set -e
+
+# death after SIGINT
+trap 'exit' INT
+
+export SILENT_MODE=1
+
+for i in 9; do
+    export BERT_MODEL=./5_training/saved_models_er_nc/roberta-base-final-$i
+    export OUTPUT_DIR_NAME=roberta-base-final-$i
+    export OUTPUT_DIR=./6_testing/results/saved_models_er_nc/${OUTPUT_DIR_NAME}
+    export TEST_RELATIONS=1
+    export TEST_DATA=./4_intermediate_data/no_context/validation/
+    mkdir -p $OUTPUT_DIR
+    ./6_testing/scripts/run.sh
+done
