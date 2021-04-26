c89b7c14569a84e4a219fd89616050fb433c71ed
Yoonsik Park <park.yoonsik@icloud.com>
2021-04-26 07:07:20 -0500

    bugfix for pipeline

--- 6_validation/scripts/pipeline/biomed_roberta_base_e_r_nc.sh
+++ 6_validation/scripts/pipeline/biomed_roberta_base_e_r_nc.sh
@@ -10,7 +10,7 @@ export SILENT_MODE=1
 for i in {1..10}; do
     export BERT_MODEL=./5_training/saved_models_r_nc/biomed-roberta-base-final-$i
     export MODEL_DIR_NAME=biomed-roberta-base-final-$i
-    export ENTITY_DATA=./6_validation/results/saved_models_r_nc/${MODEL_DIR_NAME}
+    export ENTITY_DATA=./6_validation/results/saved_models_e_nc/${MODEL_DIR_NAME}
     export TEST_DATA=./4_intermediate_data/no_context/validation/
     ./6_validation/scripts/pipeline.sh
 done
