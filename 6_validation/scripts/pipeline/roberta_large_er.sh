1a11b9b6bb7a5f69ffd002919730dcf6390a9f52
Yoonsik Park <park.yoonsik@icloud.com>
2021-04-26 07:04:07 -0500

    add pipeline scripts

---
+++ 6_validation/scripts/pipeline/roberta_large_er.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# stop on all errors
+set -e
+
+# death after SIGINT
+trap 'exit' INT
+
+export SILENT_MODE=1
+
+for i in {1..10}; do
+    export BERT_MODEL=./5_training/saved_models_er/roberta-large-final-$i
+    export MODEL_DIR_NAME=roberta-large-final-$i
+    export ENTITY_DATA=./6_validation/results/saved_models_er/${MODEL_DIR_NAME}
+    export TEST_DATA=./4_intermediate_data/context/validation/
+    ./6_validation/scripts/pipeline.sh
+done
